b5a3ab6885d97768ec3251fe7fa24eb8e34524e6
sme-long <85333514+sme-long@users.noreply.github.com>
2021-06-04 13:41:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Chào buổi chiều, Dương Hải!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
