f3e5f3f498d7af88ca75a3eedf4eed731d01163e
sme-long <85333514+sme-long@users.noreply.github.com>
2021-06-04 14:19:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 const CONFIG = {
-    introTitle: 'Chào buổi chiều, Dương Hải!',
-    introDesc: `Làm bài thi tốt',
-    btnIntro: 'Okie🙂',
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh',
-    btnNo: 'Không, bạn à',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
