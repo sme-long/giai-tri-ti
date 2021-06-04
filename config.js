7db660e17080808ce00265e3209a98af77006000
sme-long <85333514+sme-long@users.noreply.github.com>
2021-06-04 14:15:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
     introTitle: 'Chào buổi chiều, Dương Hải!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây thương`,
+    introDesc: `Làm bài thi tốt',
     btnIntro: 'Okie🙂',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
